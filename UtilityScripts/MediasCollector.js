2754a26c3bfc5fcba1684614f4ee1e9fa00a9d9b
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2021-09-17 13:43:22 +0200

    Updated media collection, as it excluded certain subtypes.

--- UtilityScripts/MediasCollector.js
+++ UtilityScripts/MediasCollector.js
@@ -1,26 +1,42 @@
 // Run this script in the browser on https://www.iana.org/assignments/media-types/media-types.xhtml#application
-JSON.stringify(Array
-    .from(document.querySelectorAll('td'))
-    .filter(td => /.+\/.+/.test(td.textContent))
-    .map(td => td.textContent)
-    .map(s => s.trim())
-    .reduce((accumulator, entry) => entry.split('/').shift() in accumulator
-            ? {
-                ...accumulator,
-                [entry.split('/').shift()]: accumulator[entry.split('/').shift()].concat(entry.split('/').pop())
-            }
-            : accumulator
-        , {
-            application: [],
-            audio: [],
-            font: [],
-            image: [],
-            message: [],
-            model: [],
-            multipart: [],
-            text: [],
-            video: []
-        })
+
+const validTypes = [
+    "application",
+    "audio",
+    "font",
+    "image",
+    "message",
+    "model",
+    "multipart",
+    "text",
+    "video",
+];
+
+JSON.stringify(validTypes
+    .map(type => [type, `table-${type}`])
+    .map(([type, id]) => [type, document.getElementById(id)])
+    .map(([type, table]) => [type, table.querySelector('tbody')])
+    .map(([type, tbody]) => [
+        type,
+        Array
+            .from(tbody.rows)
+            .map(row => row.cells[0].textContent.trim())
+            .map(subtype => subtype.split(" ")[0])
+    ])
+    .reduce((accumulator, [type, subtypes]) => ({
+        ...accumulator,
+        [type]: (accumulator.type || []).concat(subtypes)
+    }), {
+        application: [],
+        audio: [],
+        font: [],
+        image: [],
+        message: [],
+        model: [],
+        multipart: [],
+        text: [],
+        video: []
+    })
 )
 
 // Run this script in the browser on https://www.iana.org/assignments/media-type-structured-suffix/media-type-structured-suffix.xml
