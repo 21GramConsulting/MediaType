b0697b454504ff1485f497e9beb83e707f2841fd
Tamás Kálcza <calydk@gmail.com>
2022-03-22 10:45:40 +0100

    Reworded enum cases in MediaType.

--- Sources/MediaType/MediaType.swift
+++ Sources/MediaType/MediaType.swift
@@ -29,31 +29,31 @@ import Foundation
 ///
 /// ```swift
 /// MediaType.application(.other("vnd.efi.img")) // is equivalent to
-/// let mediaType: MediaType = "application/vnd.efi.img"
+/// "application/vnd.efi.img"
 /// ```
 public enum MediaType {
-  /// Creates an `application` media type.
+  /// Represents the `application` media type.
   ///
   /// Represents binary data. Common examples: `application/json`, `application/octet-stream`.
   ///
   /// For the whole family of `application` media types consult the
   /// [official IANA](https://www.iana.org/assignments/media-types/media-types.xhtml#application) documentation.
   case application(Application)
-  /// Creates an `audio` media type.
+  /// Represents the `audio` media type.
   ///
   /// Represents audible data. Common examples: `audio/ac3`, `audio/mpeg`.
   ///
   /// For the whole family of `audio` media types consult the
   /// [official IANA](https://www.iana.org/assignments/media-types/media-types.xhtml#audio) documentation.
   case audio(Audio)
-  /// Creates a `font` media type.
+  /// Represents the `font` media type.
   ///
   /// Represents font or typeface data. Common examples: `font/woff`, `font/ttf`.
   ///
   /// For the whole family of `font` media types consult the
   /// [official IANA](https://www.iana.org/assignments/media-types/media-types.xhtml#font) documentation.
   case font(Font)
-  /// Creates a `image` media type.
+  /// Represents the `image` media type.
   ///
   /// Represents image or graphical data. This includes bitmap and vector images, along with animated image formats.
   /// Common examples: `image/jpeg`, `image/apng`.
@@ -61,21 +61,21 @@ public enum MediaType {
   /// For the whole family of `image` media types consult the
   /// [official IANA](https://www.iana.org/assignments/media-types/media-types.xhtml#image) documentation.
   case image(Image)
-  /// Creates a `message` media type.
+  /// Represents the `message` media type.
   ///
   /// Represents embedded message data. Common examples: `message/rfc882`, `message/http`.
   ///
   /// For the whole family of `message` media types consult the
   /// [official IANA](https://www.iana.org/assignments/media-types/media-types.xhtml#message) documentation.
   case message(Message)
-  /// Creates a `model` media type.
+  /// Represents the `model` media type.
   ///
   /// Represents 3D modelling data. Common examples: `model/step`, `model/3mf`.
   ///
   /// For the whole family of `model` media types consult the
   /// [official IANA](https://www.iana.org/assignments/media-types/media-types.xhtml#model) documentation.
   case model(Model)
-  /// Creates a `multipart` media type.
+  /// Represents the `multipart` media type.
   ///
   /// Represents data formed from multiple components, which may have their individual media types.
   /// Common examples: `multipart/form-data`, `multipart/encrypted`.
@@ -83,14 +83,14 @@ public enum MediaType {
   /// For the whole family of `multipart` media types consult the
   /// [official IANA](https://www.iana.org/assignments/media-types/media-types.xhtml#multipart) documentation.
   case multipart(Multipart)
-  /// Creates a `text` media type.
+  /// Represents the `text` media type.
   ///
   /// Represents textual only data. Common examples: `text/css`, `text/html`.
   ///
   /// For the whole family of `text` media types consult the
   /// [official IANA](https://www.iana.org/assignments/media-types/media-types.xhtml#text) documentation.
   case text(Text)
-  /// Creates a `video` media type.
+  /// Represents the `video` media type.
   ///
   /// Represents video data. Common examples: `video/mp4`, `video/H264`.
   ///
--- UtilityScripts/MediaDocumentations.mjs
+++ UtilityScripts/MediaDocumentations.mjs
@@ -1,55 +1,55 @@
 export const mediaDocumentations = {
-    application: `  /// Creates an \`application\` media type.
+    application: `  /// Represents the \`application\` media type.
   ///
   /// Represents binary data. Common examples: \`application/json\`, \`application/octet-stream\`.
   ///
   /// For the whole family of \`application\` media types consult the
   /// [official IANA](https://www.iana.org/assignments/media-types/media-types.xhtml#application) documentation.`,
-    audio: `  /// Creates an \`audio\` media type.
+    audio: `  /// Represents the \`audio\` media type.
   ///
   /// Represents audible data. Common examples: \`audio/ac3\`, \`audio/mpeg\`.
   ///
   /// For the whole family of \`audio\` media types consult the
   /// [official IANA](https://www.iana.org/assignments/media-types/media-types.xhtml#audio) documentation.`,
-    font: `  /// Creates a \`font\` media type.
+    font: `  /// Represents the \`font\` media type.
   ///
   /// Represents font or typeface data. Common examples: \`font/woff\`, \`font/ttf\`.
   ///
   /// For the whole family of \`font\` media types consult the
   /// [official IANA](https://www.iana.org/assignments/media-types/media-types.xhtml#font) documentation.`,
-    image: `  /// Creates a \`image\` media type.
+    image: `  /// Represents the \`image\` media type.
   ///
   /// Represents image or graphical data. This includes bitmap and vector images, along with animated image formats.
   /// Common examples: \`image/jpeg\`, \`image/apng\`.
   ///
   /// For the whole family of \`image\` media types consult the
   /// [official IANA](https://www.iana.org/assignments/media-types/media-types.xhtml#image) documentation.`,
-    message: `  /// Creates a \`message\` media type.
+    message: `  /// Represents the \`message\` media type.
   ///
   /// Represents embedded message data. Common examples: \`message/rfc882\`, \`message/http\`.
   ///
   /// For the whole family of \`message\` media types consult the
   /// [official IANA](https://www.iana.org/assignments/media-types/media-types.xhtml#message) documentation.`,
-    model: `  /// Creates a \`model\` media type.
+    model: `  /// Represents the \`model\` media type.
   ///
   /// Represents 3D modelling data. Common examples: \`model/step\`, \`model/3mf\`.
   ///
   /// For the whole family of \`model\` media types consult the
   /// [official IANA](https://www.iana.org/assignments/media-types/media-types.xhtml#model) documentation.`,
-    multipart: `  /// Creates a \`multipart\` media type.
+    multipart: `  /// Represents the \`multipart\` media type.
   ///
   /// Represents data formed from multiple components, which may have their individual media types.
   /// Common examples: \`multipart/form-data\`, \`multipart/encrypted\`.
   ///
   /// For the whole family of \`multipart\` media types consult the
   /// [official IANA](https://www.iana.org/assignments/media-types/media-types.xhtml#multipart) documentation.`,
-    text: `  /// Creates a \`text\` media type.
+    text: `  /// Represents the \`text\` media type.
   ///
   /// Represents textual only data. Common examples: \`text/css\`, \`text/html\`.
   ///
   /// For the whole family of \`text\` media types consult the
   /// [official IANA](https://www.iana.org/assignments/media-types/media-types.xhtml#text) documentation.`,
-    video: `  /// Creates a \`video\` media type.
+    video: `  /// Represents the \`video\` media type.
   ///
   /// Represents video data. Common examples: \`video/mp4\`, \`video/H264\`.
   ///
