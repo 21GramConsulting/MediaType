c5e7bc52459f1f4c4f6d096a691a1f82a5e2bf72
Tamás Kálcza <calydk@gmail.com>
2022-03-23 09:29:47 +0100

    Added examples to use Font type.

---
+++ Tests/MediaTypeTests/CodeExamples/CodeExamples+Font.swift
@@ -0,0 +1,24 @@
+import XCTest
+import MediaType
+
+extension CodeExamples {
+  func test_font() {
+    let font = Font.ttf(nil, ["layout": "oat,aat"])
+    let mediaType = MediaType.font(font)
+    XCTAssertEqual(mediaType, "font/ttf;layout=oat,aat")
+  }
+
+  func test_isSupportedFont() {
+    func isSupported(font: Font) -> Bool {
+      switch font {
+      case .woff, .woff2: return true
+      case .ttf(_, let parameters) where parameters != nil: return !parameters!.isEmpty
+      default: return false
+      }
+    }
+
+    XCTAssertTrue(isSupported(font: .woff()))
+    XCTAssertTrue(isSupported(font: .ttf(nil, ["layout": "oat,aat"])))
+    XCTAssertFalse(isSupported(font: .ttf()))
+  }
+}
