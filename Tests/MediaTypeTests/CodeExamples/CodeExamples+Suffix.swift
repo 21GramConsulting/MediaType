bf8d2939e09b7eb2849e6dc365012e66f51ecd7f
Tamás Kálcza <calydk@gmail.com>
2022-03-23 11:37:51 +0100

    Formatting.

--- Tests/MediaTypeTests/CodeExamples/CodeExamples+Suffix.swift
+++ Tests/MediaTypeTests/CodeExamples/CodeExamples+Suffix.swift
@@ -3,8 +3,8 @@ import MediaType
 
 extension CodeExamples {
   func test_suffix_use_cases() {
-    XCTAssertEqual(MediaType.application(.jose(.json)),            "application/jose+json")
+    XCTAssertEqual(MediaType.application(.jose(.json)), "application/jose+json")
     XCTAssertEqual(MediaType.application(.jose(.other("custom"))), "application/jose+custom")
-    XCTAssertEqual(MediaType.image(.svg("zip")),                   "image/svg+zip")
+    XCTAssertEqual(MediaType.image(.svg("zip")), "image/svg+zip")
   }
 }
